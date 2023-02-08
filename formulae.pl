a944658ec7b2326849ea938b7d422bbf622560c6
ngabriel8 <nazar.gabriel@gmail.com>
2023-02-07 22:08:03 -0500

    and the scripts

---
+++ formulae.pl
@@ -0,0 +1,123 @@
+#!/usr/bin/perl
+
+# @(#) NMG/2WM - $Id: formulae.pl,v 1.1.1.1 2023/01/15 05:45:10 user Exp $
+use strict;
+use warnings;
+
+use Storable qw(store);
+use Data::Dumper;
+
+our $all_f_store='/tmp/all_f.store';
+
+our %ALL_F=(
+ 'Saturday' => {
+   '01-01-2022' => [
+      [ qw^A + B + g + h^ ],
+      [ qw^( A + C ) * ( e + f + g + h )^ ],
+      [ qw^C + D - a - b - c - d - HF1^ ],
+      [ qw^( A + B ) * D + g + h^ ],
+      [ qw^( A + B ) * D + e + f + g + h^ ],
+      [ qw^e + f + g + h + B^ ],
+   ],
+   '01-08-2022' => [ # powerplay = 10
+      [ qw^( A + B ) * h + h^ ],
+      [ qw^( A + B ) * h + h + A^ ],
+      [ qw^B * h + C^],
+      [ qw^( A + B ) * h + C + D^ ],
+      [ qw^( A + B ) * h + C + D + e + f + g + h - A^ ],
+      [ qw^A + B + g + h^ ],
+   ],
+   '01-15-2022' => [
+      [ qw^d - c - A^ ],
+      [ qw^B + A + h^ ],
+      [ qw^C + D - d^ ],
+      [ qw^C + D + d + g + h^ ],
+      [ qw^C + D + B + h^ ],
+      [ qw^B - A - HF1^ ],
+   ], 
+   '10-29-2022' => [
+      [ qw^B - A^ ],
+      [ qw^B + HF2^ ],
+      # BIN(A) + B^,
+      [ qw^A + B + HF1^ ],
+      [ qw^A + B + HF1 + e + f + g + h^ ],
+      [ qw^e * D + B - A - ( e + f + g + h )^ ],
+      [ qw^B - A + g + h^ ],
+   ],
+ },
+ 'Monday'    => {
+   '01-03-2022' => [
+      [ qw^B - A^ ],
+      [ qw^( g + h ) * B + A^ ],
+      [ qw^( B - A ) * ( g + h ) * ( g + h )^ ],
+      [ qw^( B - A ) * ( g + h ) * ( g + h ) + A^ ],
+      [ qw^( D * B ) - C + B - A^ ],
+      [ qw^B * ( e + f * g + h ) + g + h^ ],
+   ],
+   '01-10-2022' => [
+      [ qw^B + g + h^ ],
+      [ qw^B + A + e + f + g + h^ ],
+      [ qw^B + A + e + f + g + h - HF1^ ],
+      [ qw^D + B - A - g - h^ ],
+      [ qw^D - HF1^ ],
+      [ qw^B - A^ ],
+   ],
+   '01-17-2022' => [
+      [ qw^c + d + A^ ],
+      [ qw^C + D + c + d - g - h^ ],
+      [ qw^h * B^ ],
+      [ qw^( h * B ) + A^ ],
+      [ qw^C + D + B + e + f + g + h^ ],
+      [ qw^A + B + g + h^ ],
+   ],
+   '01-31-2022' => [
+      [ qw^c + d + e + f + g + h^ ],
+      [ qw^( B - A ) / h^ ],
+      [ qw^B + C^ ],
+      [ qw^B * h - A^ ],
+      [ qw^B + C + D - f - h^ ],
+      [ qw^( B - A ) / h - HF1^ ],
+   ],
+ },
+ 'Wednesday'  => {
+   '01-05-2022' => [
+      [ qw^A + B^ ],
+      [ qw^C - B - A^ ],
+      [ qw^C + B^ ],
+      [ qw^A + B + D + d^ ],
+      [ qw^C + D + B - A^ ],
+      [ qw^D - B^ ],
+   ],
+   '01-12-2022' => [
+      [ qw^A * B^ ],
+      [ qw^B + e + f + g + h + c + d^ ],
+      [ qw^B + e + f + g + h + c + d + A^ ],
+      [ qw^C + D - B^ ],
+      [ qw^C + D - B + c + d^ ],
+      [ qw^B * d^ ],
+   ], 
+   '02-16-2022' => [
+      [ qw^A + B + g + h^ ],
+      [ qw^( B - HF1 ) * A^ ],
+      [ qw^( g + h + B ) * A^ ],
+      [ qw^( c + d ) * d^ ],
+      [ qw^( b + c ) * B^ ],
+      [ qw^( e + f + g + h ) * d * A^ ],
+      [ qw^( c + d ) * A + A^ ],
+   ],
+   '06-29-2022' => [
+      [ qw^A + c^ ],
+      [ qw^A + B + e + f + g + h - HF1^ ],
+      [ qw^C + D + A + HF1^ ], # or
+      [ qw^e + f + B^ ],
+      [ qw^C + D + A + c + d - HF1^ ],
+      [ qw^C + D + B + A - HF2^ ],
+      [ qw^( d - c ) * h^ ],
+   ],
+ },
+);
+
+
+store(\%ALL_F, $all_f_store);
+
+1;
---
+++ wgol_driver.pl
@@ -0,0 +1,79 @@
+#!/usr/bin/perl
+
+# @(#) NMG/2WM - $Id:$
+use strict;
+use warnings;
+use File::Basename qw(basename);
+
+use Wrapper::GetoptLong;
+use lib ('.');
+use Superlotto;
+
+# in the OPTS_CONFIG hash use $obj for func. The Wrapper constructor uses obj internally to the object
+# passed to it. Command line options are saved in %opts (private). 
+# But can be used in CONFIG hash as argument to a function, an example is the get_draw option, please see below.
+
+our $sl=new Superlotto;
+
+($^O eq 'MSWin32') && ($ENV{'HOME'}='C:/cygwin64/home/user');
+
+# opt_arg_eg is opt_arg_example
+# help option will be add automatically by GetoptWrapper.pm
+
+my %OPTS_CONFIG=(
+   'process_all'      => {
+      'desc'         => 'process all draw dates.',
+      'func'         => '$obj->process_all()',
+      'opt_arg_eg'   => '',
+      'opt_arg_type' => '',
+   },
+   'load_formula'     => {
+      'desc'         => "load formulae from ALL_F hash in file: formulae.pl",
+      'func'         => q^$obj->{'dbc'}->load_formulae_to_db()^,
+      'opt_arg_eg'   => '',
+      'opt_arg_type' => '',
+   },
+   'get_draw'         => {
+      'desc'         => "followed by date in YYYY-MM-DD format. To get the drawing result for that date.",
+      'func'         => q^$obj->get_draw($opts{'get_draw'})^,
+      'opt_arg_eg'   => '<YYYY-MM-DD>',
+      'opt_arg_type' => 's',
+   },
+   'get_draw_re'      => {
+      'desc'         => "followed by date regex, underscore is wild card in the database. Get dates that match regex.",
+      'func'         => q^$obj->get_draw_re($opts{'get_draw_re'})^,
+      'opt_arg_eg'   => '<20__-01-01>', 
+      'opt_arg_type' => 's',
+   },
+   'future_draw_date' => {
+      'desc'         => "followed by date in YYYY-MM-DD format, generate suggested numbers.",
+      'func'         => q^$obj->future_draw_date($opts{'future_draw_date'})^,
+      'opt_arg_eg'   => '<YYYY-MM-DD>', 
+      'opt_arg_type' => 's',
+   },
+   'gen_factors'      => {
+      'desc'         => "followed by date in YYYY-MM-DD format, generate factors hash ref.",
+      'func'         => q^$obj->{'dbc'}->mk_factors_str($opts{'gen_factors'})^,
+      'opt_arg_eg'   => '<YYYY-MM-DD>', 
+      'opt_arg_type' => 's',
+   },
+   'add_month'        => {
+      'desc'         => "followed by <file-name(s)> in MonYYYY.txt format, e.g. Jan2023.txt  to load into the database.",
+      'func'         => q^$obj->{'dbc'}->add_month_to_db($opts{'add_month'})^,
+      'opt_arg_eg'   => '<MonYYYY.txt>', 
+      'opt_arg_type' => 's{,}',
+   },
+);
+
+my $wgol=new Wrapper::GetoptLong(\%OPTS_CONFIG, $sl);
+$wgol->run_getopt();
+my $rc=$wgol->execute_opt();
+if(ref($rc))
+  {
+  use Data::Dumper;
+  print Dumper $rc;
+  }
+else
+  {
+  print $rc;
+  }
